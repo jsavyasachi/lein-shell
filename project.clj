e666fbce595d86bd109740f9a4fe5d1748456dbd
Savyasachi <jsavyasachi@gmail.com>
2026-06-16 23:08:14 -0700

    build: bump provided leiningen-core 2.12.0 -> 2.13.0

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
   ;; provided (non-transitive) so cljdoc can resolve leiningen.core.* for API docs
-  :profiles {:provided {:dependencies [[leiningen-core "2.12.0"]]}})
+  :profiles {:provided {:dependencies [[leiningen-core "2.13.0"]]}})
