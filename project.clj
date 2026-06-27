cd79cf29a8ff621b5f0696e3979ecf99b4709f9d
Savyasachi <jsavyasachi@gmail.com>
2026-06-26 21:54:19 -0700

    ci: add tag-triggered Clojars release workflow and env-cred deploy config

--- project.clj
+++ project.clj
@@ -5,4 +5,8 @@
             :url "https://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
   ;; provided (non-transitive) so cljdoc can resolve leiningen.core.* for API docs
-  :profiles {:provided {:dependencies [[leiningen-core "2.13.0"]]}})
+  :profiles {:provided {:dependencies [[leiningen-core "2.13.0"]]}}
+  :deploy-repositories [["clojars" {:url "https://repo.clojars.org"
+                                    :username :env/clojars_username
+                                    :password :env/clojars_password
+                                    :sign-releases false}]])
