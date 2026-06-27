ec38f5a249ab588a1083f475561e7bc00435a02b
Savyasachi <jsavyasachi@gmail.com>
2026-06-26 21:06:40 -0700

    chore: specify EPL version 1.0 and https in license metadata

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject net.clojars.savya/lein-shell "1.0.2"
   :description "Call shell from within Leiningen."
   :url "https://github.com/jsavyasachi/lein-shell"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "Eclipse Public License 1.0"
+            :url "https://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
   ;; provided (non-transitive) so cljdoc can resolve leiningen.core.* for API docs
   :profiles {:provided {:dependencies [[leiningen-core "2.13.0"]]}})
